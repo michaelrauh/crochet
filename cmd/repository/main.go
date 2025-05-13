da13287427bdd0bec52892b6a04a7a87b3c209d6
michaelrauh <michaelrauh@ymail.com>
2025-05-12 21:04:19 -0400

    move handler

--- cmd/repository/main.go
+++ cmd/repository/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"crochet/internal/handler"
 	"crochet/internal/httpserver"
 	"crochet/pkg/config"
 
@@ -10,7 +9,7 @@ import (
 
 func main() {
 	fx.New(
-		fx.Provide(config.Load, httpserver.NewRouter, handler.NewHandler),
-		fx.Invoke(handler.RegisterRoutes, httpserver.StartServer),
+		fx.Provide(config.Load, httpserver.NewRouter, NewHandler),
+		fx.Invoke(RegisterRoutes, httpserver.StartServer),
 	).Run()
 }
